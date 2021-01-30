960d5a3f0b1d13a4c563439ece93dab378d80ecd
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2021-01-30 12:43:20 +0100

    Parche header Login User

--- resources/views/layouts/home/_header-buttons.blade.php
+++ resources/views/layouts/home/_header-buttons.blade.php
@@ -1,24 +1,56 @@
 <div style="height: 148px;"></div>
-<div class="mx-auto">
-    <h3>
-        <strong class="rounded" style="color: white;background-color: #0000007a">
-            ¿Aún no eres miembro?
-        </strong>
-    </h3>
-</div>
-<div>
-    <a href="{{ route('registry') }}" class="btn btn btn-success btn-lg shadow p-3" style="width: 263px; border-color: white;">
-        <strong>
-            ¡REGÍSTRATE AHORA!
-        </strong>
-    </a>
-</div>
-<br>
-<div>
-    <a href="{{ route('download.launcher') }}" class="btn btn-primary btn-lg shadow p-2" style="width: 375px; border-color: white;">
-        <strong>
-            Descargar Juego
-        </strong>
-    </a>
-</div>
-
+@if (!auth()->check())
+    <div id="bottonesHome">
+        <div class="mx-auto">
+            <h3>
+                <strong class="rounded" style="color: white;background-color: #0000007a">
+                    ¿Aún no eres miembro?
+                </strong>
+            </h3>
+        </div>
+        <div>
+            <a href="{{ route('registry') }}" class="btn btn btn-success btn-lg shadow p-3"
+                style="width: 263px; border-color: white;">
+                <strong>
+                    ¡REGÍSTRATE AHORA!
+                </strong>
+            </a>
+        </div>
+        <br>
+        <div>
+            <a href="{{ route('download.launcher') }}" class="btn btn-primary btn-lg shadow p-2"
+                style="width: 375px; border-color: white;">
+                <strong>
+                    Descargar Juego
+                </strong>
+            </a>
+        </div>
+    </div>
+@else
+    <div id="bottonesHome" style="visibility: hidden">
+        <div class="mx-auto">
+            <h3>
+                <strong class="rounded" style="color: white;background-color: #0000007a">
+                    ¿Aún no eres miembro?
+                </strong>
+            </h3>
+        </div>
+        <div>
+            <a href="{{ route('registry') }}" class="btn btn btn-success btn-lg shadow p-3"
+                style="width: 263px; border-color: white;">
+                <strong>
+                    ¡REGÍSTRATE AHORA!
+                </strong>
+            </a>
+        </div>
+        <br>
+        <div>
+            <a href="{{ route('download.launcher') }}" class="btn btn-primary btn-lg shadow p-2"
+                style="width: 375px; border-color: white;">
+                <strong>
+                    Descargar Juego
+                </strong>
+            </a>
+        </div>
+    </div>
+@endif
