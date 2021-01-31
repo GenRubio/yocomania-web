881ef94a948bb4fd4a0052c3b47680a0eb795501
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2021-01-31 15:41:35 +0100

    parche

--- resources/views/components/fanart/fanart.blade.php
+++ resources/views/components/fanart/fanart.blade.php
@@ -5,8 +5,8 @@
             @foreach ($fanart as $imagen)
                 <div class="col-md-6 col-lg-4 mb-4">
                     <div class="card border-0 transform-on-hover">
-                        <a class="lightbox" href="{{ $imagen->link }}"><img
-                                src="{{ $imagen->link }}" class="card-img-top", height="250px"></a>
+                        <a class="lightbox" href=".{{ $imagen->link }}"><img
+                                src=".{{ $imagen->link }}" class="card-img-top", height="250px"></a>
                         <div class="card-body">
                             <p class="text-muted card-text"><strong style="color:#3490dc;">{{ $imagen->descripcion }}</strong></p>
                         </div>
